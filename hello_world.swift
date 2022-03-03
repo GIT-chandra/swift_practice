66f6b188746a6fb1c1d55b3401d291d7a224711c
Angom Geetchandra <angomgeetchandra@gmail.com>
2022-03-03 20:06:10 +0530

    completed basics page

--- hello_world.swift
+++ hello_world.swift
@@ -1,3 +1,7 @@
+/*
+Learning from https://docs.swift.org/swift-book/LanguageGuide/TheBasics.html#
+
+
 let EXCLAMATION = "!"
 var name: String
 name = "Geet!"
@@ -26,4 +30,61 @@ var num1=60, num2=9; var num3 = num1 + num2
 
 print("\(num1) + \(num2) = \(num3)")    // NOICE! Also comment can begin anywhere in a line it seems
 
-// TODO: verify system architecture/word-size based on Max value of Int compared with Int32/Int64
+*/
+// fatalError("Yooooo!")   // make it stop for no reason
+
+// can we use tuples as variable too? The site only gave examples of constants
+var myTup = (daNumba: 69, message: "Noice!")
+print("\(myTup.daNumba)")
+print("\(myTup.message == "Nice")")     // let's see if we can use expressions inside these substitution things
+
+// can we do operations between an optional and non optional of the same type?
+
+var couldBeInt = Int("123")
+var definitelyInt = 1
+
+/* This one didn't work
+print(couldBeInt + definitelyInt)
+*/
+print(couldBeInt! + definitelyInt)  // okay, just need to force unwrap
+
+// but what if I try force unwrap on something that doesn't have a value
+/*
+print(Int("hey there")!)
+Yikes! This one was a fatal error and a bunch of nasty messages were printed */
+
+// and do vars play well with consts
+let definitelyIntAndUnchanging = 3
+print(definitelyInt + definitelyIntAndUnchanging)
+
+let myString = "120"
+// var myString = "120.45"
+// var myString = "World Hello"
+if let theNumber = Int(myString) {  // don't type == here. I learnt the hard way!
+    print("Int() says: Hell yeah, I can convert \(myString). It is just \(theNumber)")
+}   // also, can I write else in the next line? We'll find out
+else {
+    print("Int() says: What's this !!!? \(myString)")
+}
+
+// btw, string use only double quotes or single as well
+/*
+print('hi')
+Nope. Needs to be double quotes always, it seems*/
+
+
+let defInt: Int! = 10
+let maybeInt: Int? = 10
+let convertedInt: Int = defInt
+print(convertedInt)
+
+// but can you use without assigning to a var or const
+print(20 + defInt)  // Yup, it works
+
+/* whereas this doesn't work
+print(20 + maybeInt)
+So then why even use Implicitly Unwrapped optionals? Can't just Int be used? I guess more to learn 
+*/
+
+
+
---
+++ random_snippets/detect_arch.swift
@@ -0,0 +1,9 @@
+if Int.max == Int32.max {
+    print("This is a 32 bit system!")
+}
+
+if Int.max == Int64.max {
+    print("This is a 64 bit system!")
+}
+
+// probably there's some else or elif type of thing in swift too, but haven't reached that part :D
